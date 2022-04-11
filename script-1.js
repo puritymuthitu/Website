cc2b49fd7cf2578b31204e6fc6a6f0792024007e
puritymuthitu <97535851+puritymuthitu@users.noreply.github.com>
2022-04-11 16:03:41 -0400

    index 4

--- script-1.js
+++ script-1.js
@@ -5,28 +5,28 @@ const alert = document.getElementById("option1")
 
 $(document).ready(function(){
     $("#generate equation").click(function(){
-var num1 = Math.floor(Math.random() *21)
-var num2 = Math.floor(Math.random() *21)
-var dummyAnswer1 = Math.floor(Math.random() *21)
-var dummyAnswer2 = Math.floor(Math.random() *21)
-var allAnswers = [];
-var switchAnswers = [];
+        var num1 = Math.floor(Math.random() *21)
+        var num2 = Math.floor(Math.random() *21)
+        var dummyAnswer1 = Math.floor(Math.random() *21)
+        var dummyAnswer2 = Math.floor(Math.random() *21)
+        var allAnswers = [];
+        var switchAnswers = [];
 
-answer = num1 + num2;
+        answer = num1 + num2;
 
-document.getElementById("num1").innerHTML = num1;
-document.getElementById("num2").innerHTML = num2;
+        document.getElementById("num1").innerHTML = num1;
+        document.getElementById("num2").innerHTML = num2;
 
-allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+        allAnswers = [answer, dummyAnswer1, dummyAnswer2];
 
-for(i = allAnswers.length;i--;){
-    switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
+        for(i = allAnswers.length;i--;){
+            switchAnswers.push(allAnswers.slice(Math.floor(Math.random() * (i + 1)), 1)[0]);
 
-}
-option1.innerHTML = switchAnswers[0];
-option2.innerHTML = switchAnswers[1];
-option3.innerHTML = switchAnswers[2];
-}
+        }
+        option1.innerHTML = switchAnswers[0];
+        option2.innerHTML = switchAnswers[1];
+        option3.innerHTML = switchAnswers[2];
+    }
 
 option1.addEventAlertWarning("click", function(){
     if (option1.innerHTML == answer)
